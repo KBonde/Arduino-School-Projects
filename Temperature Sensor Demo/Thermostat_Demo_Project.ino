2534758bdc285af39ec8a2ea86aa016756c28bb7
Kristian Bonde Nielsen <kristianbonde@live.dk>
2018-01-09 15:31:10 +0100

    Cleaned up a bit - also testing how file replacement works

--- Temperature Sensor Demo/Thermostat_Demo_Project.ino
+++ Temperature Sensor Demo/Thermostat_Demo_Project.ino
@@ -1,6 +1,6 @@
 #include <math.h> //include library for log function
-#include "VernierLib.h"
 
+//Variables for temperature calculation
 float rawCount; //create global variable for reading from A/D converter (0-1023)
 float temperature; //create global variable for temperature in Celsius
 
@@ -9,41 +9,47 @@ float avgTemp = 0;
 float totalTemp = 0;
 unsigned int counter = 0;
 
-//Pins
+//Pins are assigned here
 unsigned short LED_PIN = 13;
 
 void setup() {
-  Serial.begin(9600); //setup communication to display
-
+  //Setup serial communication so results appear in the Serial Console
+  Serial.begin(9600); 
+  
   //Pin setup
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  counter++; 
+  counter++; //Increment counter for correct loop number 
   
   rawCount=analogRead(A2); //read one data value (0-1023)
   temperature=thermistor(rawCount); //calculate temperature
-  Serial.print("Tmp: ");
-  Serial.println(temperature,1); // display temperature to one decimal)
-
+  
   //Turn on lamp if temp is above 30
   if(temperature > 30) {
-    digitalWrite(LED_PIN, LOW);                      
+    digitalWrite(LED_PIN, HIGH); //Set pin to high (on)                      
   } else {
-    digitalWrite(LED_PIN, HIGH);    
+    digitalWrite(LED_PIN, LOW);  //Set pin to low (off)  
   }
 
   //Calculate and display avg temp
   totalTemp += temperature;
-  avgTemp = totalTemp/counter;   
+  avgTemp = totalTemp / counter;
 
+  //Print current temp
+  Serial.print("Tmp: ");
+  Serial.println(temperature,1);
+
+  //Print average temp
   Serial.print("Avg: ");
   Serial.println(avgTemp, 1);
-  
+
+  //Delay for 0.5 seconds - we measure the temp twice each second
   delay(500);  
 }
 
+//The following function is made by some guys at MIT - not me
 //This function calculates temperature from raw count
 float thermistor(int raw) {
   const float resistor=15000; //initialize value of fixed resistor in Vernier shield
